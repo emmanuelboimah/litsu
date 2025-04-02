f8fb12ad42ed92034c7b5d63ec6f77acc4219abf
boimahcharles <dianak@ame.edu.lr>
2025-04-02 05:16:53 +0000

    second commit

--- workspaces/urls.py
+++ workspaces/urls.py
@@ -8,12 +8,5 @@ urlpatterns = [
     path('litsu-wp/', admin.site.urls),
     
     path('', views.Home_Page, name='home'),
-    path('chapters/ <str:id>', views.Chapters, name='chapters'),
-    path('news updates', views.News, name='news'),
-    path('news/ <str:id>', views.News_Detalte, name='news_detalte'),
-    path('membership form', views.Membership_Form, name='membership_form'),
-    path('about us', views.About_Us, name='about_us'),
-    path('contact us', views.Contact_Us, name='contact_us'),
-    path('leadership/ <str:id>', views.Leadership, name='leadership'),
 
 ]
