298b770787c953aa7a4a317c0392c76126f55a76
boimahcharles <dianak@ame.edu.lr>
2025-04-02 04:01:28 +0000

    first commit

--- workspaces/settings.py
+++ workspaces/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import dj_database_url
 
+import os
 from environ import Env
 env = Env()
 Env.read_env()
@@ -123,7 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static-root")
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
